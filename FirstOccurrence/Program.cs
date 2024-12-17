35ce9004534f278b1d1b3c08b0be444938a4423a
Tural Agayev <115562103+agayev97@users.noreply.github.com>
2024-12-17 22:18:56 +0400

    Add files via upload

---
+++ FirstOccurrence/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOccurrence
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            string haystack1 = "sadbutsad";
+            string needle1 = "sad";
+            int result1 = solution.StrStr(haystack1, needle1);
+            Console.WriteLine($"Input: haystack = \"{haystack1}\", needle = \"{needle1}\""); 
+            Console.WriteLine($"Output: {result1}");
+            Console.WriteLine($"Explanation: \"{needle1}\" occurs at index 0 and 6 in \"{haystack1}\". The first occurrence is at index {result1}.\n");
+
+
+            string haystack2 = "leetcode";
+            string needle2 = "leeto";
+            int result2 = solution.StrStr(haystack2, needle2);
+            Console.WriteLine($"Input: haystac = \"{haystack1}\", needle = \"{needle2}\"");
+            Console.WriteLine($"Output: {result2}");
+            Console.WriteLine($"Explanation: \"{needle2}\"  did not occur in \"{haystack2}\", so we return {result2}.\n");
+
+            Console.ReadKey();
+
+        }
+
+        public class Solution
+        {
+            public int StrStr(string haystack, string needle)
+            {
+                if (needle.Length == 0) return 0;
+
+                for (int i = 0; i <= haystack.Length - needle.Length; i++)
+                {
+                    if (haystack.Substring(i, needle.Length) == needle)
+                    {
+                        return i;
+                    }
+                }
+
+                return -1;
+            }
+
+        }
+    }
+}
