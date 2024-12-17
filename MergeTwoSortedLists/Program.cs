11bbdd799dab8e82375241cb5da29690c6e860a1
Tural Agayev <115562103+agayev97@users.noreply.github.com>
2024-12-17 22:19:38 +0400

    Add files via upload

---
+++ MergeTwoSortedLists/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeTwoSortedLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new program();
+
+            LinkedListNode  list1 = MergeTwoSortedLists.program.CreateList(new int[] { 1, 2, 4 });
+        }
+    }
+
+    public class program
+    {
+        public 
+    }
+
+}
